b6028014acf2c9b083772edd95b8e42628d0955b
Mayukh <mayukhchatterjee2023@gmail.com>
2023-02-19 13:16:09 +0530

    added two files

---
+++ dom.js
@@ -0,0 +1,12 @@
+
+
+    let loo=document.getElementsByClassName("list-group-item")
+   
+    loo[2].style.color='green'
+    loo[2].textContent= "kauchy"
+
+   for (let index = 0; index < loo.length; index++) {
+    loo[index].style.backgroundColor='yellow'
+    loo[index].style.fontWeight='bold'
+    
+   }
